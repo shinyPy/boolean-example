ec4e263a8977a89b55305ddaa8bee09c4da6a5b6
shin <71328419+shinyPy@users.noreply.github.com>
2022-08-08 14:52:34 +0800

    Create boolean.php
    example

---
+++ boolean.php
@@ -0,0 +1,15 @@
+<?php 
+    $a = True;
+    $b = False;
+
+    if ( $a == True) {
+      echo 'Variabel $a bernilai True';
+    }
+
+    echo "<br/>";
+    if ( $b== False ) {
+      echo 'Variabel $b bernilai False'
+    }
+
+
+?>
